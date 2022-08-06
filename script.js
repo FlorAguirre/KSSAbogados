c18e98068345174e847df4aee9587b777f0762f8
FlorAguirre <floraguirre1598@gmail.com>
2022-08-06 18:25:39 -0400

    Funcionalidad en Especialidades

--- script.js
+++ script.js
@@ -1,5 +1,113 @@
-function openImg(){
-    var image = document.getElementById('image');
-    var source = image.src;
-    window.open(source);
-}
+function mostrar() {
+
+        document.getElementById('texto').style.display = 'block'
+        document.getElementById('flechaarriba').style.display = 'block'
+        document.getElementById('flechaabajo').style.display ="none"
+    /* else if (texto1 === texto1) {
+        document.getElementById('texto1').style.display = 'block'
+    } else if (texto2 === texto2) {
+        document.getElementById('texto2').style.display = 'block'
+    } else if (texto3 === texto3) {
+        document.getElementById('texto3').style.display = 'block'
+    } else if (texto4 === texto4) {
+        document.getElementById('texto4').style.display = 'block'
+    } else if (texto5 === texto5) {
+        document.getElementById('texto5').style.display = 'block'
+    } else if (texto6 === texto6) {
+        document.getElementById('texto6').style.display = 'block'
+    } else if (texto7 === texto7) {
+        document.getElementById('texto7').style.display = 'block'
+    } else if (texto8 === texto8) {
+        document.getElementById('texto8').style.display = 'block'
+    } else if (texto9 === texto9) {
+        document.getElementById('texto9').style.display = 'block'
+    } */}
+
+
+function ocultar() {
+        document.getElementById('texto').style.display = 'none'
+        document.getElementById('flechaarriba').style.display = 'none'
+        document.getElementById('flechaabajo').style.display ="block"
+    /*  else if (texto1 === texto1) {
+        document.getElementById('texto1').style.display = 'none'
+    } else if (texto2 === texto2) {
+        document.getElementById('texto2').style.display = 'none'
+    } else if (texto3 === texto3) {
+        document.getElementById('texto3').style.display = 'none'
+    } else if (texto4 === texto4) {
+        document.getElementById('texto4').style.display = 'none'
+    } else if (texto5 === texto5) {
+        document.getElementById('texto5').style.display = 'none'
+    } else if (texto6 === texto6) {
+        document.getElementById('texto6').style.display = 'none'
+    } else if (texto7 === texto7) {
+        document.getElementById('texto7').style.display = 'none'
+    } else if (texto8 === texto8) {
+        document.getElementById('texto8').style.display = 'none'
+    } else if (texto9 === texto9) {
+        document.getElementById('texto9').style.display = 'none'
+    } */}
+
+    function mostrar1(){
+        document.getElementById('texto1').style.display = 'block'
+    }
+
+    function ocultar1(){
+        document.getElementById('texto1').style.display = 'none'
+    }
+    function mostrar2(){
+        document.getElementById('texto2').style.display = 'block'
+    }
+
+    function ocultar2(){
+        document.getElementById('texto2').style.display = 'none'
+    }
+    function mostrar3(){
+        document.getElementById('texto3').style.display = 'block'
+    }
+
+    function ocultar3(){
+        document.getElementById('texto3').style.display = 'none'
+    }
+    function mostrar4(){
+        document.getElementById('texto4').style.display = 'block'
+    }
+
+    function ocultar4(){
+        document.getElementById('texto4').style.display = 'none'
+    }
+    function mostrar5(){
+        document.getElementById('texto5').style.display = 'block'
+    }
+
+    function ocultar5(){
+        document.getElementById('texto5').style.display = 'none'
+    }
+    function mostrar6(){
+        document.getElementById('texto6').style.display = 'block'
+    }
+
+    function ocultar6(){
+        document.getElementById('texto6').style.display = 'none'
+    }
+    function mostrar7(){
+        document.getElementById('texto7').style.display = 'block'
+    }
+
+    function ocultar7(){
+        document.getElementById('texto7').style.display = 'none'
+    }
+    function mostrar8(){
+        document.getElementById('texto8').style.display = 'block'
+    }
+
+    function ocultar8(){
+        document.getElementById('texto8').style.display = 'none'
+    }
+    function mostrar9(){
+        document.getElementById('texto9').style.display = 'block'
+    }
+
+    function ocultar9(){
+        document.getElementById('texto9').style.display = 'none'
+    }
