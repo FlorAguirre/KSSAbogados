ace23781c9514236dc923de09a074c4da8148cd3
FlorAguirre <floraguirre1598@gmail.com>
2022-08-07 01:15:37 -0400

    Seccion Contactanos

--- script.js
+++ script.js
@@ -3,111 +3,111 @@ function mostrar() {
         document.getElementById('texto').style.display = 'block'
         document.getElementById('flechaarriba').style.display = 'block'
         document.getElementById('flechaabajo').style.display ="none"
-    /* else if (texto1 === texto1) {
-        document.getElementById('texto1').style.display = 'block'
-    } else if (texto2 === texto2) {
-        document.getElementById('texto2').style.display = 'block'
-    } else if (texto3 === texto3) {
-        document.getElementById('texto3').style.display = 'block'
-    } else if (texto4 === texto4) {
-        document.getElementById('texto4').style.display = 'block'
-    } else if (texto5 === texto5) {
-        document.getElementById('texto5').style.display = 'block'
-    } else if (texto6 === texto6) {
-        document.getElementById('texto6').style.display = 'block'
-    } else if (texto7 === texto7) {
-        document.getElementById('texto7').style.display = 'block'
-    } else if (texto8 === texto8) {
-        document.getElementById('texto8').style.display = 'block'
-    } else if (texto9 === texto9) {
-        document.getElementById('texto9').style.display = 'block'
-    } */}
+ }
 
 
 function ocultar() {
         document.getElementById('texto').style.display = 'none'
         document.getElementById('flechaarriba').style.display = 'none'
         document.getElementById('flechaabajo').style.display ="block"
-    /*  else if (texto1 === texto1) {
-        document.getElementById('texto1').style.display = 'none'
-    } else if (texto2 === texto2) {
-        document.getElementById('texto2').style.display = 'none'
-    } else if (texto3 === texto3) {
-        document.getElementById('texto3').style.display = 'none'
-    } else if (texto4 === texto4) {
-        document.getElementById('texto4').style.display = 'none'
-    } else if (texto5 === texto5) {
-        document.getElementById('texto5').style.display = 'none'
-    } else if (texto6 === texto6) {
-        document.getElementById('texto6').style.display = 'none'
-    } else if (texto7 === texto7) {
-        document.getElementById('texto7').style.display = 'none'
-    } else if (texto8 === texto8) {
-        document.getElementById('texto8').style.display = 'none'
-    } else if (texto9 === texto9) {
-        document.getElementById('texto9').style.display = 'none'
-    } */}
+   }
 
     function mostrar1(){
         document.getElementById('texto1').style.display = 'block'
+        document.getElementById('flechaarriba1').style.display = 'block'
+        document.getElementById('flechaabajo1').style.display ="none"
     }
 
     function ocultar1(){
         document.getElementById('texto1').style.display = 'none'
+        document.getElementById('flechaarriba1').style.display = 'none'
+        document.getElementById('flechaabajo1').style.display ="block"
     }
     function mostrar2(){
         document.getElementById('texto2').style.display = 'block'
+        document.getElementById('flechaarriba2').style.display = 'block'
+        document.getElementById('flechaabajo2').style.display ="none"
     }
 
     function ocultar2(){
         document.getElementById('texto2').style.display = 'none'
+        document.getElementById('flechaarriba2').style.display = 'none'
+        document.getElementById('flechaabajo2').style.display ="block"
     }
     function mostrar3(){
         document.getElementById('texto3').style.display = 'block'
+        document.getElementById('flechaarriba3').style.display = 'block'
+        document.getElementById('flechaabajo3').style.display ="none"
     }
 
     function ocultar3(){
         document.getElementById('texto3').style.display = 'none'
+        document.getElementById('flechaarriba3').style.display = 'none'
+        document.getElementById('flechaabajo3').style.display ="block"
     }
     function mostrar4(){
         document.getElementById('texto4').style.display = 'block'
+        document.getElementById('flechaarriba4').style.display = 'block'
+        document.getElementById('flechaabajo4').style.display ="none"
     }
 
     function ocultar4(){
         document.getElementById('texto4').style.display = 'none'
+        document.getElementById('flechaarriba4').style.display = 'none'
+        document.getElementById('flechaabajo4').style.display ="block"
     }
     function mostrar5(){
         document.getElementById('texto5').style.display = 'block'
+        document.getElementById('flechaarriba5').style.display = 'block'
+        document.getElementById('flechaabajo5').style.display ="none"
     }
 
     function ocultar5(){
         document.getElementById('texto5').style.display = 'none'
+        document.getElementById('flechaarriba5').style.display = 'none'
+        document.getElementById('flechaabajo5').style.display ="block"
     }
     function mostrar6(){
         document.getElementById('texto6').style.display = 'block'
+        document.getElementById('flechaarriba6').style.display = 'block'
+        document.getElementById('flechaabajo6').style.display ="none"
     }
 
     function ocultar6(){
         document.getElementById('texto6').style.display = 'none'
+        document.getElementById('flechaarriba6').style.display = 'none'
+        document.getElementById('flechaabajo6').style.display ="block"
     }
     function mostrar7(){
         document.getElementById('texto7').style.display = 'block'
+        document.getElementById('flechaarriba7').style.display = 'block'
+        document.getElementById('flechaabajo7').style.display ="none"
     }
 
     function ocultar7(){
         document.getElementById('texto7').style.display = 'none'
+        document.getElementById('flechaarriba7').style.display = 'none'
+        document.getElementById('flechaabajo7').style.display ="block"
     }
     function mostrar8(){
         document.getElementById('texto8').style.display = 'block'
+        document.getElementById('flechaarriba8').style.display = 'block'
+        document.getElementById('flechaabajo8').style.display ="none"
     }
 
     function ocultar8(){
         document.getElementById('texto8').style.display = 'none'
+        document.getElementById('flechaarriba8').style.display = 'none'
+        document.getElementById('flechaabajo8').style.display ="block"
     }
     function mostrar9(){
         document.getElementById('texto9').style.display = 'block'
+        document.getElementById('flechaarriba9').style.display = 'block'
+        document.getElementById('flechaabajo9').style.display ="none"
     }
 
     function ocultar9(){
         document.getElementById('texto9').style.display = 'none'
+        document.getElementById('flechaarriba9').style.display = 'none'
+        document.getElementById('flechaabajo9').style.display ="block"
     }
